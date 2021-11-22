5d51bbbdb5f1e279cd2c0efbfffa5ad0aa1d57f5
Askarunro <paskar.y.a@gmail.com>
2021-11-22 22:48:59 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,7 @@
+import '../css/common.css';
+import '../css/01-gallery.css';
+import '../css/03-feedback.css';
+
 const refs = {
   form: document.querySelector('.feedback-form'),
   input: document.querySelector('.feedback-form input'),
@@ -8,10 +12,10 @@ refs.form.addEventListener('submit', onFormSubmit);
 refs.input.addEventListener('input', onInputInput);
 // refs.textarea.addEventListener('input', onTextareaInput);
 const formData = {};
-localStorage.setItem('feedback-form-state', JSON.stringify({ formData }));
+localStorage.setItem('100', JSON.stringify({ formData }));
 refs.form.addEventListener('input', evt => {
   formData[evt.target.name] = evt.target.value;
-  localStorage.getItem('feedback-form-state', JSON.stringify({ formData }));
+  localStorage.setItem('feedback-form-state', JSON.stringify({ formData }));
 
   console.log(formData);
 });
@@ -26,7 +30,7 @@ function onFormSubmit(evt) {
 
 function onInputInput(evt) {
   const email = evt.target.value;
-  localStorage.getItem('feedback-form-state1', email);
+  localStorage.setItem('feedback-form-state1', email);
   console.log(email);
 }
 
