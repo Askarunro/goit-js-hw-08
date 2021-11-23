9cd001691b48d89a72c846a613e46ffd21fb1265
Askarunro <paskar.y.a@gmail.com>
2021-11-23 23:22:00 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,15 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 console.log(player);
-player.on('timeupdate', function (data) {
-  const a = data.seconds;
-  localStorage.setItem('current-Time', JSON.stringify(a));
-});
-const b = localStorage.getItem('current-Time');
-player.setCurrentTime(b);
+
+player.on('timeupdate', throttle(currentData, 1000));
+
+function currentData(data) {
+  const currentSeconds = data.seconds;
+  localStorage.setItem('current-Time', JSON.stringify(currentSeconds));
+}
+
+const continueView = localStorage.getItem('current-Time');
+player.setCurrentTime(continueView);
