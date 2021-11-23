8658896349e983494195cc67ee1543a3783eb815
Askarunro <paskar.y.a@gmail.com>
2021-11-23 23:10:30 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
-
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+console.log(player);
+player.on('timeupdate', function (data) {
+  const a = data.seconds;
+  localStorage.setItem('current-Time', JSON.stringify(a));
+});
+const b = localStorage.getItem('current-Time');
+player.setCurrentTime(b);
