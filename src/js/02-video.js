17d5ce45d0b8595e4f32fba3935f7115add73cb5
Askarunro <paskar.y.a@gmail.com>
2021-11-23 23:25:26 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,8 +8,8 @@ player.on('timeupdate', throttle(currentData, 1000));
 
 function currentData(data) {
   const currentSeconds = data.seconds;
-  localStorage.setItem('current-Time', JSON.stringify(currentSeconds));
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(currentSeconds));
 }
 
-const continueView = localStorage.getItem('current-Time');
+const continueView = localStorage.getItem('videoplayer-current-time');
 player.setCurrentTime(continueView);
