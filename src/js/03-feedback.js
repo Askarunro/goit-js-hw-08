2c9f65f4ef6d63f820068e68c00292d1d509274e
Askarunro <paskar.y.a@gmail.com>
2021-11-27 22:27:46 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -84,3 +84,55 @@ function populateTextarea() {
     refs.textarea.value = savedMessage;
   }
 }
+
+// import throttle from 'lodash.throttle';
+// import '../css/common.css';
+// import '../css/01-gallery.css';
+// import '../css/03-feedback.css';
+
+// const refs = {
+//   form: document.querySelector('.feedback-form'),
+//   input: document.querySelector('.feedback-form input'),
+//   textarea: document.querySelector('.feedback-form textarea'),
+// };
+
+// refs.form.addEventListener('submit', onFormSubmit);
+// // refs.input.addEventListener('input', onInputInput);
+// // refs.textarea.addEventListener('input', onTextareaInput);
+// populateTextarea();
+// let formData = {};
+
+// if (localStorage.getItem('feedback-form-state')) {
+//   populateTextarea();
+//   console.log('Hi');
+//   refs.form.addEventListener('input', throttle(onFormInput, 500));
+// } else {
+//   refs.form.addEventListener('input', throttle(onFormInput, 500));
+// }
+
+// function onFormInput(evt) {
+//   // formData[evt.target.name] = evt.target.value;
+//   // console.log(formData);
+//   localStorage.setItem(
+//     'feedback-form-state',
+//     JSON.stringify({
+//       email: refs.input.value,
+//       message: refs.textarea.value,
+//     }),
+//   );
+// }
+
+// function onFormSubmit(evt) {
+//   evt.preventDefault();
+//   evt.currentTarget.reset();
+//   localStorage.removeItem('feedback-form-state');
+// }
+
+// function populateTextarea() {
+//   const savedMessage = localStorage.getItem('feedback-form-state');
+//   if (savedMessage) {
+//     const dataNew = JSON.parse(savedMessage);
+//     refs.input.value = dataNew.email;
+//     refs.textarea.value = dataNew.message;
+//   }
+// }
